2155a8c1c2e37dd144dcf699035eb959fa931f35
Talon Daniels <talondaniels@gmail.com>
2014-06-17 21:58:34 -0600

    add website and remove code

--- src/main/java/com/shaneisrael/st/prefs/PreferencesException.java
+++
@@ -1,9 +0,0 @@
-package com.shaneisrael.st.prefs;
-public class PreferencesException extends Exception {
-
-    private static final long serialVersionUID = -4976383656388815952L;
-
-    public PreferencesException(String msg) {
-        super(msg);
-    }
-}
